5ecbedaec6cabdc59fb1804260d93ffa5c5d4e9b
jpsferreira <joao.ferreira@digestaid.health>
2024-09-11 17:44:10 +0100

    add bindings to spatial tensors. add umat handler class

--- hyper_surrogate/materials.py
+++ hyper_surrogate/materials.py
@@ -42,6 +42,20 @@ class Material(SymbolicHandler):
         """Material stiffness tensor in symbolic form."""
         return self.cmat_tensor(self.pk2_symb)
 
+    def sigma_symb(self, f: sym.Matrix) -> Any:
+        """Cauchy stress tensor in symbolic form."""
+        return self.pushforward_2nd_order(self.pk2_symb, f)
+
+    def smat_symb(self, f: sym.Matrix) -> Any:
+        """Material stiffness tensor in spatial form."""
+        return self.pushforward_4th_order(self.cmat_symb, f)
+
+    def cauchy(self, f: sym.Matrix) -> Any:
+        return self.reduce_2nd_order(self.sigma_symb(f))
+
+    def tangent(self, f: sym.Matrix) -> Any:
+        return self.reduce_4th_order(self.smat_symb(f))
+
     def pk2(self) -> Any:
         """Second Piola-Kirchhoff stress tensor generator of numerical form."""
         return self.lambdify(self.pk2_symb, *self.parameters)
@@ -49,6 +63,14 @@ class Material(SymbolicHandler):
     def cmat(self) -> Any:
         """Material stiffness tensor generator of numerical form."""
         return self.lambdify(self.cmat_symb, *self.parameters)
+
+    def sigma(self, f: sym.Matrix) -> Any:
+        """Cauchy stress tensor generator of numerical form."""
+        return self.lambdify(self.sigma_symb(f), *self.parameters)
+
+    def smat(self, f: sym.Matrix) -> Any:
+        """Material stiffness tensor generator of numerical form."""
+        return self.lambdify(self.smat_symb(f), *self.parameters)
 
 
 class NeoHooke(Material):
